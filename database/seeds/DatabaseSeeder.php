118c5178fe51a2b5a78a99eb8ce4930726f5c84f
serii <seriiburduja@gmail.com>
2022-11-17 18:05:08 +0200

    feat: create taxonomy

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,14 +14,14 @@
             $this->call(RolesTableSeeder::class);
             $this->call(UsersTableSeeder::class);
             $this->call(UserRolesTableSeeder::class);
-            $this->call(AttributeGroupsSeeder::class);
-            $this->call(AttributeProductsSeeder::class);
-            $this->call(AttributeValuesSeeder::class);
-            $this->call(BrandsSeeder::class);
-            $this->call(CategoriesSeeder::class);
-            $this->call(CurrenciesSeeder::class);
-            $this->call(GalleriesSeeder::class);
-            $this->call(ProductsSeeder::class);
+            /* $this->call(AttributeGroupsSeeder::class); */
+            /* $this->call(AttributeProductsSeeder::class); */
+            /* $this->call(AttributeValuesSeeder::class); */
+            /* $this->call(BrandsSeeder::class); */
+            /* $this->call(CategoriesSeeder::class); */
+            /* $this->call(CurrenciesSeeder::class); */
+            /* $this->call(GalleriesSeeder::class); */
+            /* $this->call(ProductsSeeder::class); */
             /* $this->call(RelatedProductsSeeder::class); */
             /* $this->call(OrdersSeeder::class); */
             /* $this->call(AdminOrderProductsSeeder::class); */
