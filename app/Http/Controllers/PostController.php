4e377d4e9310528106c52152884fc782a20220c2
serii <seriiburduja@gmail.com>
2022-11-26 17:23:03 +0200

    postController: pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,9 +9,23 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
-  public function index()
+  public function index(Request $request)
   {
-    $query = Post::query()->paginate(9);
-    return PostResource::collection($query);
+    $query = Post::query();
+    $limit = $request->limit;
+    $offset = $request->offset;
+
+    if (isset($request->post_category_id) && $request->post_category_id !== "0") {
+      $query = $query->where('post_category_id', '=', $request->post_category_id);
+    }
+
+    $result = PostResource::collection($query->offset($offset)->orderBy('updated_at')->limit($limit)->get());
+
+    return response()->json(
+      [
+        'data' => $result,
+        'total' => $query->count()
+      ]
+    );
   }
 }
