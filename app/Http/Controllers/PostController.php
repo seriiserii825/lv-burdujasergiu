8e541d22e7c0d424c2072a103b1a18b71d75d31c
serii <seriiburduja@gmail.com>
2022-11-26 19:23:43 +0200

    search: finish

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,9 +14,14 @@ class PostController extends Controller
     $query = Post::query();
     $limit = $request->limit;
     $offset = $request->offset;
+    $s = $request->search;
 
     if (isset($request->post_category_id) && $request->post_category_id !== "0") {
       $query = $query->where('post_category_id', '=', $request->post_category_id);
+    }
+
+    if(isset($s) && $s !== "") {
+      $query = $query->where('title', 'like', '%' . $s . '%');
     }
 
     $result = PostResource::collection($query->offset($offset)->orderBy('updated_at')->limit($limit)->get());
